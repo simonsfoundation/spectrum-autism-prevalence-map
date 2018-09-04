41365e1eba1930d3db786b457d6588fefbb7f834
JD Godchaux <jd@nijel.org>
2018-09-04 13:20:49 -0400

    Added card on click to show data for each study on the map

--- spectrum/autism_prevalence_map/static/autism_prevalence_map/js/map.js
+++ spectrum/autism_prevalence_map/static/autism_prevalence_map/js/map.js
@@ -8,9 +8,6 @@ $(document).ready(function (){
     // data holder
     let studies = null;
 
-    // pin holder 
-    let pins = null;
-
     // map projection
     const projection = d3.geoKavrayskiy7()
         .scale(270)
@@ -81,7 +78,18 @@ $(document).ready(function (){
 		        .attr("cy", function (d) { return projection(d.geometry.coordinates)[1]; })
                 .attr("r", 5)
                 .style("fill", "#1fcbec")
-                .classed("pin", true);
+                .style("stroke", "#fff")
+                .style("stroke-width", "0")
+                .classed("pin", true)
+                .on("click", viewMoreInfo)
+                .on("mouseover", function(d) {
+                    d3.select(this).style("cursor", "pointer"); 
+                    d3.select(this).style("stroke-width", "2");
+                })
+                .on("mouseout", function(d) {
+                    d3.select(this).style("cursor", "default");
+                    d3.select(this).style("stroke-width", "0");
+                });
         });
     }
 
@@ -90,7 +98,7 @@ $(document).ready(function (){
     const svg_zoom = d3.select('#svg');
 
     const zoom = d3.zoom()
-        .scaleExtent([1/2, 4])
+        .scaleExtent([0.75, 3.375])
         .on("zoom", zoomed);
 
     svg_zoom.call(zoom);
@@ -98,34 +106,83 @@ $(document).ready(function (){
     // define zooming function for zooming map
     function zoomed() {
         g_zoom.attr('transform', `translate(${d3.event.transform.x},  ${d3.event.transform.y}) scale(${d3.event.transform.k})`);
+        // set pin zizes based on zoom level
+        scalePins(d3.event.transform.k);
     };
 
     // define function zoom transiton 
     function transition(zoomLevel) {
         svg_zoom.transition()
-            .delay(100)
-            .duration(700)
+            .duration(500)
             .call(zoom.scaleBy, zoomLevel);
     }
+    
+    // define the radius change for points as we soom in annd out
+    function scalePins(k) {
+        
+        console.log(k);
+
+        // calculate new radius
+        const new_radius = 5 - k;
+
+        // select all pins and apply new radius in transition with zoom
+        d3.selectAll('.pin').transition()
+            .duration(100)
+            .attr("r", new_radius);
+
+        // select all pins and apply new stroke width on hover
+        d3.selectAll('.pin')
+            .on("mouseover", function(d) {
+                d3.select(this).style("cursor", "pointer"); 
+                d3.select(this).style("stroke-width", new_radius/2.5);            
+            });
+    }
+
 
     // set up listeners for zoom
     d3.selectAll('button').on('click', function() {
-        // zoom in 0.2 each time
+        // zoom in 0.5 each time
         if (this.id === 'zoom-in') {
             transition(1.5); 
+            scalePins(-1);
         }
-        // zoom out 0.2 each time
+        // zoom out 0.5 each time
         if (this.id === 'zoom-out') {
-            transition(0.5); 
+            transition(0.7);
+            scalePins(1); 
         }
         // return to initial state
         if (this.id === 'zoom_init') {
             svg_zoom.transition()
-                .delay(100)
-                .duration(700)
+                .duration(500)
                 .call(zoom.scaleTo, 1); 
         }
     });
+
+    // function for launching more information card
+    function viewMoreInfo(d) {
+        // add data to card
+        const card_title = d.properties.authors + ' ' + d.properties.year
+        $("#card-title").text(card_title);
+        $("#card-country").text(d.properties.country);
+        $("#card-area").text(d.properties.area);
+        $("#card-population").text(d.properties.population);
+        $("#card-age").text(d.properties.age);
+        $("#card-diagnostic_criteria").text(d.properties.diagnostic_criteria);
+        $("#card-pct_with_normal_iq").text(d.properties.pct_with_normal_iq);
+        $("#card-gender_ratio").text(d.properties.gender_ratio);
+        $("#card-prevalence_rate").text(d.properties.prevalence_rate);
+        $("#card-confidence_interval").text(d.properties.confidence_interval);
+        
+        // show card
+        $("#more-information-card").css("display", "block");
+    }
+
+    // listener to close card
+    $("#card-close").click(function(){
+        $("#more-information-card").css("display", "none");
+    });
+    
 
 
 });
