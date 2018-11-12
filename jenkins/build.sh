689da722ba2be3afbfef7f5719eb96f06206eb02
Rishiraj Rana <rrana@simonsfoundation.org>
2018-11-12 14:54:57 -0500

    ci process updated

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -17,14 +17,16 @@ fi
 
 if $(grep -q \$ANSIBLE_VAULT jenkins/ssl.config) ; then 
   ansible-vault decrypt jenkins/ssl.config --vault-password-file=~/\ /ansible.vault ;
+  mv jenkins/ssl.config .ebextensions/ssl.config
 fi
 
 if [[ -z "${OUTPUT_ENV}" ]]; then
   exit 1
 fi
 
-eb deploy "${OUTPUT_ENV}" --timeout 10
+eb deploy "${OUTPUT_ENV}" --timeout 30
 
 if ! $(grep -q \$ANSIBLE_VAULT jenkins/ssl.config) ; then 
+  mv .ebextensions/ssl.config jenkins/ssl.config
   ansible-vault encrypt jenkins/ssl.config --vault-password-file=~/\ /ansible.vault ;
 fi
