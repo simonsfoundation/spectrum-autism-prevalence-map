60b369df8b4173dffed7b2f83cda140a5a221756
Michael Kranz <mkranz@simonsfoundation.org>
2019-05-10 12:15:38 -0400

    Changing response['status'] to 200 instead of OK in views.py, defining ALLOWED_HOSTS the same regardless of the value of DEBUG 	modified:   spectrum/autism_prevalence_map/views.py 	modified:   spectrum/spectrum/settings.py

--- spectrum/spectrum/settings.py
+++ spectrum/spectrum/settings.py
@@ -29,10 +29,7 @@ SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ["DJANGO_DEBUG"]
 
-if DEBUG == "True":
-    ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
-else:
-    ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost").split(",")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost").split(",")
 
 # Application definition
 
