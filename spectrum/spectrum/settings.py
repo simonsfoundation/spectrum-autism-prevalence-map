a03420cc7ccc06818f46e8994074fbf1f349b4fa
Rishiraj Rana <rrana@simonsfoundation.org>
2021-09-20 22:19:58 -0400

    fixed issue with STATIC_ROOT

--- spectrum/spectrum/settings.py
+++ spectrum/spectrum/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = os.environ["DJANGO_STATIC_URL"]
 
-STATIC_ROOT = "/var/www/html" + os.environ["DJANGO_STATIC_URL"]
+STATIC_ROOT = "/var/www/html"
 
 STATICFILES_DIRS = [
     (f"{BASE_DIR}" + "/autism_prevalence_map/")
