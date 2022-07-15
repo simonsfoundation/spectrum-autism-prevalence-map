06fc7505bd6fd39cce14bb4dddb9734be429ec9e
Jinfeng Ye <yjfnyc@gmail.com>
2022-07-15 14:07:37 -0400

    specwww-756: changed the login path name

--- spectrum/spectrum/urls.py
+++ spectrum/spectrum/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^submarine/', admin.site.urls),
     url(r'^', include('autism_prevalence_map.urls')),
 ]
