250fe52b23ccb91ef3098092cc7d860d4bc7cd52
mattga11ant <gallantcodes@gmail.com>
2025-01-30 11:13:08 -0700

    TTWWW-471: merge mike/TTWWW-468 branch in

--- spectrum/autism_prevalence_map/static/autism_prevalence_map/tailwind.config.js
+++ spectrum/autism_prevalence_map/static/autism_prevalence_map/tailwind.config.js
@@ -7,6 +7,18 @@ module.exports = {
     ],
     theme: {
         fontSize: {
+            'sm': ['0.75rem', { // 12px
+                lineHeight: '120%',
+                letterSpacing: '0.16em',
+            }],
+            'sm2': ['0.688rem', { // 11px
+                lineHeight: '120%',
+                letterSpacing: '0.16em',
+            }],
+            'md1': ['1.438rem', { // 23px
+                lineHeight: '100%',
+                letterSpacing: '0.06em',
+            }]
         },
         extend: {
             fontFamily: {
@@ -15,6 +27,7 @@ module.exports = {
                 'serif-text': ['GothiaSerifText', 'serif'],
             },
             borderRadius: {
+                '3.5': '0.875rem', // 14px
             },
             letterSpacing: {
             },
@@ -25,20 +38,42 @@ module.exports = {
             screens: {
             },
             spacing: {
+                '62' : '15.5rem', // 248px
+                '17.5' : '4.375rem', // 70px
+                '10': '2.5rem', // 40px
+                '4.7': '1.188rem', // 19px
+                '4.5': '1.125rem', // 18px
+                '4': '1rem', // 16px
+                '3.5': '0.875rem', // 14px
+                '1.5': '0.375rem', // 6px
+                '1.2': '0.3rem', // 4.8px
+            },
+            maxWidth: {
+                'container': '103rem',
+                'logo': '13.55rem'
             },
             colors: {
                 'red': '#910E1C',
                 'blue': '#0B6BC3',
                 'navy': '#003049',
                 'med-navy': '#2E5367',
+                'light-navy-3': '#6c8291',
                 'light-navy-2': '#DFE8EC',
                 'tan': '#FEF9EE',
                 'dark-tan': '#cac8c1',
                 'dark-gray': '#2D2D2D',
+<<<<<<< HEAD
                 'med-gray': '#CCC',
                 'light-gray': '#F4F5F6 ',
+=======
+                'light-gray': '#F4F5F6',
+                'black': '#000',
+                'white': '#FFF'
             },
-
+            flex: {
+                'search': '0.95 1 0%'
+>>>>>>> origin/mike/TTWWW-468
+            },
         }
     },
     plugins: [
