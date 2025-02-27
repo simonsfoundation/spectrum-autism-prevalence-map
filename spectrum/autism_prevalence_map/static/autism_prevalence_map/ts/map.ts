28f10006ed0b8065703527458bd8c1ba6aa0b227
mattga11ant <gallantcodes@gmail.com>
2025-02-27 08:15:05 -0700

    TTWWW-472: updated the welcome message copy based on Sams feedback

--- spectrum/autism_prevalence_map/static/autism_prevalence_map/ts/map.ts
+++ spectrum/autism_prevalence_map/static/autism_prevalence_map/ts/map.ts
@@ -808,8 +808,8 @@ export function ttInitMap() {
 
         // welcome card content to reset to
         const welcomeCardContent = 
-            '<h1 class="mt-24 lg:mt-25.5 xl:mt-46.5 mb-3.25 max-w-welcome font-serif text-2md font-bold leading-7.2 text-med-navy">Welcome to the Global Autism Prevalence Map</h1>' +
-            '<p class="mb-9 max-w-welcome font-serif text-sm3 leading-4.5 text-dark-gray2">Data studies will be displayed in this side panel. Click any dot on the map to view detailed information.</p>' +
+            '<h1 class="mt-24 lg:mt-25.5 xl:mt-46.5 mb-3.25 max-w-welcome font-serif text-2md font-bold leading-7.2 text-med-navy">Welcome to the Global Autism Prevalence Map.</h1>' +
+            '<p class="mb-9 max-w-welcome font-serif text-sm3 leading-4.5 text-dark-gray2">Click on any dot on the map to view detailed information about a study. Data from that study will be displayed in this panel.</p>' +
             '<a href="/about/" class="inline-block w-learn py-2.5 bg-med-navy rounded-sm text-sm font-bold text-light-navy-2 uppercase text-center no-underline">Learn More</a>';
 
         // function to clear pinned dot when the clear filters button is clicked, or filters are changed.
