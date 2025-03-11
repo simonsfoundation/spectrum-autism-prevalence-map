1e623a9fe8e10bb8d5ebd6c76a5741c59147c992
mattga11ant <gallantcodes@gmail.com>
2025-03-11 08:14:48 -0600

    TTWWW-571: updates to allow multiple pulication links to appear in the info card

--- spectrum/autism_prevalence_map/static/autism_prevalence_map/ts/map.ts
+++ spectrum/autism_prevalence_map/static/autism_prevalence_map/ts/map.ts
@@ -906,26 +906,26 @@ export function ttInitMap() {
 
             // Build an array of publication link HTML strings
             let links = [];
-            let linkClasses = 'block pl-4 text-4 text-blue tracking-2 leading-6.25 no-underline rounded-sm2-b';
+            let linkClasses = 'block pl-4 text-4 text-blue font-semibold tracking-2 leading-6.25 no-underline rounded-sm2-b';
 
             if (d.properties.link1title && d.properties.link1url) {
                 links.push('<a href="' + d.properties.link1url + '" target="_blank" class="' + linkClasses + '">' + d.properties.link1title + '</a>');
             }
             if (d.properties.link2title && d.properties.link2url) {
-                links.push('<a href="' + d.properties.link2url + '" target="_blank" class="' + linkClasses + '">' + d.properties.link2title + '</a>');
+                links.push('<a href="' + d.properties.link2url + '" target="_blank" class="' + linkClasses + ' mt-0.5">' + d.properties.link2title + '</a>');
             }
             if (d.properties.link3title && d.properties.link3url) {
-                links.push('<a href="' + d.properties.link3url + '" target="_blank" class="' + linkClasses + '">' + d.properties.link3title + '</a>');
+                links.push('<a href="' + d.properties.link3url + '" target="_blank" class="' + linkClasses + ' mt-0.5">' + d.properties.link3title + '</a>');
             }
             if (d.properties.link4title && d.properties.link4url) {
-                links.push('<a href="' + d.properties.link4url + '" target="_blank" class="' + linkClasses + '">' + d.properties.link4title + '</a>');
+                links.push('<a href="' + d.properties.link4url + '" target="_blank" class="' + linkClasses + ' mt-0.5">' + d.properties.link4title + '</a>');
             }
 
             // add the publication links into an absolutely positioned div at the bottom of the parent while the main #info-card has overflow: scroll
             if (links.length > 0) {
                 let linksHTML = links.join('');
                 linksHTML = linksHTML.replace('>Spectrum', '><em>Spectrum</em>');
-                const containerHTML = '<div id="publication-button-wrap" class="absolute bottom-0 left-0 w-full py-1.75 bg-white border-t-light-gray2 border-t-0.5 rounded-sm2-b">' + linksHTML + '</div>';
+                const containerHTML = '<div id="publication-button-wrap" class="absolute bottom-0 left-0 w-full py-3.25 bg-white border-t-light-gray2 border-t-0.5 rounded-sm2-b">' + linksHTML + '</div>';
                 $('#info-card-container').append(containerHTML);
             }
         }
